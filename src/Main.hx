802d8867aacd5cc75a49a3ba95be3c5e743c8b8b
Asher Hoeveler <contact@hecancanyou.xyz>
2022-09-26 00:00:40 -0500

    monke

---
+++ src/Main.hx
@@ -0,0 +1,42 @@
+package;
+
+import haxe.Timer;
+import haxe.Resource;
+import haxe.io.Bytes;
+import sys.thread.Thread;
+import naturalName.Name;
+
+
+class Main {
+	static macro function getDefined(key:String):haxe.macro.Expr {
+		return macro $v{haxe.macro.Context.definedValue(key)}
+	}
+	static function main():Void {
+		var monkes = 0;
+		var threads = 0;
+		var monkimg:Bytes = Resource.getBytes("monke");
+
+		for (_ in 0...Std.parseInt(getDefined("threads"))) {
+			Thread.create(() -> {
+				while (true) {
+					MonkeMultiply.Monke.worker(monkimg, './[${monkes}].jpg');
+					monkes++;
+					if (Sys.args()[0]!= "--debug") {
+						Sys.print("-MONKE");
+					}
+				}
+			});
+			threads++;
+			if (Sys.args()[0] == "--debug") Sys.print('${threads} threads started    \r');
+		}
+		//do nothing while monkeys invade the hard drive
+		if (Sys.args()[0] != "--debug") {
+			while (true) {}
+		}
+		var oMonkes = 0;
+		new Timer(100).run = () -> {
+			Sys.print('${monkes-oMonkes} monkes per second (${threads} threads)                                \r');
+			oMonkes = monkes;
+		}
+	}
+}
