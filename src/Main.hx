a680f4ea865b8b87944c291e1a8a3b086764c058
Asher Hoeveler <contact@hecancanyou.xyz>
2022-09-29 07:53:20 -0500

    forgor

--- src/Main.hx
+++ src/Main.hx
@@ -1,12 +1,5 @@
 package;
 
-import haxe.crypto.Base64;
-import haxe.Timer;
-import haxe.Resource;
-import haxe.io.Bytes;
-import sys.thread.Thread;
-
-
 class Main {
 	private static macro function getDefined(key:String):haxe.macro.Expr {
 		return macro $v{haxe.macro.Context.definedValue(key)}
